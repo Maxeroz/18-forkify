07012c06e9b0787ee642c113e2e1995b1a53de2a
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-11-10 21:13:06 +0400

    Implementing Pagination - Part 1

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,9 +8,9 @@ import 'regenerator-runtime/runtime';
 
 ///////////////////////////////////////
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function () {
   try {
@@ -44,8 +44,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3) Render results
-
-    resultsView.render(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
   } catch (err) {
     console.log(err);
   }
